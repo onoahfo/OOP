72448b4ac6432f13dab2c9a348cc5486f6220860
Noah <onoahfo@gmail.com>
2019-06-24 06:01:06 -0400

    functional programming example

---
+++ OOJ.js
@@ -0,0 +1,41 @@
+// create a function that concole logs any argument given
+function display(content){
+    console.log(content);
+}
+
+
+// create a function that takes a name(string) as a parameter
+// and returns "hi my name is" parameter given
+
+function name (name){
+    return "Hi, my name is " + name + ".";
+}
+
+
+
+
+
+// create 3 functions that describe the powers of a ninja
+// you would like to create
+
+function firePower (ability){
+    return "Ninja can use " + ability+ ".";
+}
+
+function waterPower (ability){
+    return "Ninja can use " + ability + ".";
+}
+
+function earthPower (ability){
+    return "Ninja can use " + ability + ".";
+}
+
+
+display(name("itachi"));
+display(firePower("fire spray"))
+display(waterPower("splash"))
+display(earthPower("earth wall"))
+
+
+
+
